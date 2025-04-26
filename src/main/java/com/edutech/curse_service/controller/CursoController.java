8722c54c6ec9c5ebbd2c70c530482e4f27a4f5f3
dDiazcL <diea.diaz@duocuc.cl>
2025-04-25 20:57:59 -0400

    Tercer Punto: Proyecto

--- src/main/java/com/edutech/curse_service/controller/CursoController.java
+++ src/main/java/com/edutech/curse_service/controller/CursoController.java
@@ -9,12 +9,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.edutech.curse_service.model.Curso;
 import com.edutech.curse_service.service.CursoService;
 
 @RestController
+@RequestMapping("/api/cursos")
 public class CursoController {
     @Autowired
     private CursoService cursoService;
